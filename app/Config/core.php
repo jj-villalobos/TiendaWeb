f582741bcf459811808ebc2b300438a19bd02bf5
unknown <b06882@105-03.ecci.ucr.ac.cr>
2014-10-27 09:48:41 -0600

    config

--- app/Config/core.php
+++ app/Config/core.php
@@ -355,7 +355,7 @@ Configure::write('debug', 1);
  *       Please check the comments in bootstrap.php for more info on the cache engines available
  *       and their settings.
  */
-$engine = 'File';
+$engine = 'Apc';
 
 // In development mode, caches should expire quickly.
 $duration = '+999 days';
